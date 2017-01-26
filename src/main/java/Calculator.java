fe4fab27bed8b11e90e007fe3fafe00791bc6db1
ii4q7j <ii4q7j@gmail.com>
2017-01-26 21:06:54 +0300

    My second commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,36 @@
  * Created by dmitriybala on 26.01.17.
  */
 public class Calculator {
+
+
+    public static int calculate(Double numberOne, Character operand, Double numberTwo) {
+
+        Integer result;
+
+        switch (operand) {
+            case '+':
+                result = (int) (numberOne + numberTwo);
+                break;
+            case '-':
+                result = (int) (numberOne - numberTwo);
+                break;
+            case '*':
+                result = (int) (numberOne * numberTwo);
+                break;
+            case '/':
+                result = (int) (numberOne / numberTwo);
+                break;
+            default:
+                result = Integer.MIN_VALUE;
+        }
+
+        if (result.toString().length() > 8) {
+            System.out.println("Result is too big to show");
+            return Integer.MIN_VALUE;
+        }
+
+        return result;
+
+
+    }
 }
